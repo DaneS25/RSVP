69a90d0730031df9232aca836e4be4884d874c31
DaneS25 <Dane.Tyler.Sorensen@htomail.com>
2024-10-06 10:23:41 +1300

    form sends data to my email

--- rsvp/src/Form.js
+++ rsvp/src/Form.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
-export const Form = ({ onSubmit }) => {
-return (
-<form onSubmit={onSubmit}>
-  <div className="form-group">
-      <label className='label-text-name' htmlFor="name">Name</label>
-    <input className="form-control-name" id="name" />
-  </div> 
-  
-  <div className="form-group">
-    <label className='label-text-email' htmlFor="email">Email address</label>
-    <input type="email" className="form-control-email" id="email"
-     placeholder="name@example.com" 
-    />
-</div>
-  <div className="form-group">
-    <button className="form-control-submit" type="submit">
-      Submit
-    </button>
-</div>
-</form>
-);
+export const Form = () => {
+  const form = useRef();
+
+  // Function to send email via EmailJS
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_wqp0hv4', 'template_c3qdemo', form.current, '3_FB-mibcvcaR-QGj')
+      .then((result) => {
+          console.log(result.text);
+          alert('RSVP sent successfully!');
+      }, (error) => {
+          console.log(error.text);
+          alert('An error occurred, please try again.');
+      });
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <div className="form-group">
+        <label className='label-text-name' htmlFor="name">Name</label>
+        <input className="form-control-name" id="name" name="user_name" />
+      </div> 
+
+      <div className="form-group">
+        <label className='label-text-email' htmlFor="email">Email address</label>
+        <input type="email" className="form-control-email" id="email" name="user_email" placeholder="name@example.com" />
+      </div>
+
+      <div className="form-group">
+        <button className="form-control-submit" type="submit">
+          Submit
+        </button>
+      </div>
+    </form>
+  );
 };
+
 export default Form;
